605b0a188bc4b5db38c6735a5604c9e0e23a0953
angelorc <angelo@rcstart.it>
2023-08-24 11:29:26 +0200

    add sinfonia endpoint

--- x/incentives/keeper/grpc_query.go
+++ x/incentives/keeper/grpc_query.go
@@ -227,3 +227,30 @@ func (q Querier) filterByPrefixAndDenom(ctx sdk.Context, prefixType []byte, deno
 	})
 	return pageRes, gauges, err
 }
+
+func (k Keeper) LockedByGaugeID(goCtx context.Context, req *types.LockedByGaugeIDRequest) (*types.LockedByGaugeIDResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	gauge, err := k.GetGaugeByID(ctx, req.GaugeID)
+	if err != nil {
+		return &types.LockedByGaugeIDResponse{}, err
+	}
+
+	denom := gauge.DistributeTo.Denom
+
+	locksByDenomCache := make(map[string][]lockuptypes.PeriodLock)
+	locks := k.getDistributeToBaseLocks(ctx, *gauge, locksByDenomCache)
+	lockSum := lockuptypes.SumLocksByDenom(locks, denom)
+
+	if lockSum.IsZero() {
+		return &types.LockedByGaugeIDResponse{
+			Denom:   denom,
+			LockSum: "0",
+		}, nil
+	}
+
+	return &types.LockedByGaugeIDResponse{
+		Denom:   denom,
+		LockSum: lockSum.String(),
+	}, nil
+}
